b9a3cbaa30fa9428419e4de985cafa2764d93deb
Marc Cavigli <mcavigli@gmail.com>
2019-04-29 22:42:53 +0000

    Fixed efficiency

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,7 +2,7 @@
 
 /**
  * check_cycle - checks to see if a list is in an endless loop or cycle
- * list: the list to check
+ * @list: the list to check
  * Return: 0 if no cycle is detected, 1 if there is a cycle
  */
 
@@ -10,8 +10,7 @@ int check_cycle(listint_t *list)
 {
 	listint_t *reg, *doub;
 
-	reg = list;
-	doub = list;
+	reg = list, doub = list;
 
 	while (reg != NULL && doub != NULL)
 	{
