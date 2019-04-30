db6a5c5b28d392e27f7ec3ad6b9a60917fc51bdb
Marc Cavigli <mcavigli@gmail.com>
2019-04-30 21:32:36 +0000

    Changed location of spaces

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,13 +2,10 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz ", end='')
+            print("FizzBuzz", end=' ')
         elif i % 5 == 0:
-            if i == 100:
-                print("Buzz $", end='')
-                break
-            print("Buzz ", end='')
+            print("Buzz", end=' ')
         elif i % 3 == 0:
-            print("Fizz ", end='')
+            print("Fizz", end=' ')
         else:
-            print("{} ".format(i), end='')
+            print("{}".format(i), end=' ')
