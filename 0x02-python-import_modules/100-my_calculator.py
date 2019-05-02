ee84eee77b77666ffece826f03f816538d6ec391
Marc Cavigli <mcavigli@gmail.com>
2019-05-02 21:06:33 +0000

    Only imported argv from sys

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 from calculator_1 import add, sub, mul, div
+
+
 if __name__ == "__main__":
-    argc = len(sys.argv) - 1
+    argc = len(argv) - 1
     if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    operator = sys.argv[2]
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
+    operator = argv[2]
+    a = int(argv[1])
+    b = int(argv[3])
     if operator == "+":
         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     elif operator == "-":
