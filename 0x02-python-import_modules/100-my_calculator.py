4e8f42a096aaf7f4a041f886b08af9f43e196438
Marc Cavigli <mcavigli@gmail.com>
2019-05-02 21:00:07 +0000

    Added format specifiers

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,19 +8,17 @@ if __name__ == "__main__":
     if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
     operator = sys.argv[2]
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-
     if operator == "+":
-        print("{} + {} = {}".format(a, b, add(a, b)))
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     elif operator == "-":
-        print("{} - {} = {}".format(a, b, sub(a, b)))
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     elif operator == "*":
-        print("{} * {} = {}".format(a, b, mul(a, b)))
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     elif operator == "/":
-        print("{} / {} = {}".format(a, b, div(a, b)))
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
