d675fa341912d94a08632055123a5931d878ead6
Marc Cavigli <mcavigli@gmail.com>
2019-05-02 19:45:31 +0000

    Made sure the code wouldn't execute if imported

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import sys
-argc = len(sys.argv)
-sum = 0
-for i in range(1, argc):
-    sum += int(sys.argv[i])
-print(sum)
+
+
+if __name__ == "__main__":
+    argc = len(sys.argv)
+    sum = 0
+    for i in range(1, argc):
+        sum += int(sys.argv[i])
+    print(sum)
