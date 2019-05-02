f9378a20df110852b3100b9b756c0688c5ef6a20
Marc Cavigli <mcavigli@gmail.com>
2019-05-02 20:49:30 +0000

    Changed name of variable to operator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,17 +9,17 @@ if __name__ == "__main__":
         print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
         exit(1)
 
-    op = sys.argv[2]
+    operator = sys.argv[2]
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    if op == "+":
+    if operator == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif op == "-":
+    elif operator == "-":
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif op == "*":
+    elif operator == "*":
         print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif op == "/":
+    elif operator == "/":
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
