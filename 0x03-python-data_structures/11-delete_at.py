9f14e5baf630e59ee5c76cba5610f254aacec5d0
Marc Cavigli <mcavigli@gmail.com>
2019-05-07 21:22:54 +0000

    Changed or to and for conditional

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,6 +2,6 @@
 def delete_at(my_list=[], idx=0):
     if not my_list:
         return
-    if idx >= 0 or idx < len(my_list):
+    if idx >= 0 and idx < len(my_list):
         del my_list[idx]
     return my_list
