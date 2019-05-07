dcda777c48573673c29828316a6e08c582c8c3f3
Marc Cavigli <mcavigli@gmail.com>
2019-05-07 19:27:37 +0000

    Changed condition from if idx > len of the list to idx >= len of the list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     if idx < 0:
         return new_list
-    if idx > len(new_list):
+    if idx >= len(new_list):
         return new_list
     new_list[idx] = element
     return new_list
