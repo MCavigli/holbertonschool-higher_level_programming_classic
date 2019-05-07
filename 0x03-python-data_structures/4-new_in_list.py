e7fd71b314aaa4705c70e373d8c64a0765460c5b
Marc Cavigli <mcavigli@gmail.com>
2019-05-07 19:08:47 +0000

    Pushed Task 4 to directory

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if idx < 0:
+        return new_list
+    if idx > len(new_list):
+        return new_list
+    new_list[idx] = element
+    return new_list
