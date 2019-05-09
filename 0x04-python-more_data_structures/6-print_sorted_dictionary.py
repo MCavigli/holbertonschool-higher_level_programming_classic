b83d53b0690dc55fe88fd8841ee5c14a1e7a312b
Marc Cavigli <mcavigli@gmail.com>
2019-05-09 20:34:03 +0000

    Changed code so it'll work with Task 8. Removed .items() attribute, which seemed to cause problems

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    for i in sorted(a_dictionary.keys()):
+        print("{}: {}".format(i, a_dictionary[i]))
     #    new = dict(a_dictionary)
-    for key, val in sorted(a_dictionary.items()):
-        print("{}: {}".format(key, val))
+    #    for key, val in sorted(a_dictionary.items()):
+    #        print("{}: {}".format(key, val))
