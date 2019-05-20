6ea13034321ec179a59748de4664fbb68446fcd4
Marc Cavigli <mcavigli@gmail.com>
2019-05-20 20:40:38 +0000

    Added TypeError for exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,5 +6,5 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as e:
+    except (TypeError, ValueError) as e:
         print("Exception: {}".format(e), file=sys.stderr)
