4c847d51d4ee9ba20ee5e17d2a4aeed6966072e1
Marc Cavigli <mcavigli@gmail.com>
2019-05-20 19:55:16 +0000

    Pushed Task 3 to directory

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    result = 0
+    print("Inside result: ", end='')
+    try:
+        result = a / b
+    except:
+        result = None
+    finally:
+        print("{}".format(result))
+        return result
