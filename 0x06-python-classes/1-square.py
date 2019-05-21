a8739c3def7eb1a2389c4d93be49528863fa7331
Marc Cavigli <mcavigli@gmail.com>
2019-05-21 23:12:44 +0000

    Pushed Task 1 to directory

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""defines a class called Square"""
+
+
+class Square:
+
+    """Represents a square"""
+
+    def __init__(self, size):
+
+        """Initializes the data
+        Args:
+            size: size of the square
+        """
+
+        self.__size = size
