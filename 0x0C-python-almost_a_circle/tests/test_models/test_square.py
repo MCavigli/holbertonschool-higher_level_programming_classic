8ce3a25bf91e48a08861fb29ec688027320faadb
Marc Cavigli <mcavigli@gmail.com>
2019-06-13 04:50:05 +0000

    Added x and y tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -72,6 +72,82 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(Square.__str__
                          (self.s4), "[Square] (14) 1/2 - 3")
 
+    def test_10_06_dict(self):
+        """Checks the dictionary method"""
+
+        dict1 = self.s1.to_dictionary()
+        self.assertDictEqual(dict1, {'size': 5, 'id': 11, 'x': 0, 'y': 0})
+        dict2 = self.s2.to_dictionary()
+        self.assertDictEqual(dict2, {'size': 7, 'id': 12, 'x': 0, 'y': 0})
+        dict3 = self.s3.to_dictionary()
+        self.assertDictEqual(dict3, {'size': 2, 'id': 13, 'x': 2, 'y': 0})
+        dict4 = self.s4.to_dictionary()
+        self.assertDictEqual(dict4, {'size': 3, 'id': 14, 'x': 1, 'y': 2})
+
+    def test_10_07_x_check(self):
+        """Checks x"""
+
+        self.assertEqual(self.s1.x, 0)
+        self.assertEqual(self.s2.x, 0)
+        self.assertEqual(self.s3.x, 2)
+        self.assertEqual(self.s4.x, 1)
+
+    def test_10_08_y_check(self):
+        """Checks y"""
+
+        self.assertEqual(self.s1.y, 0)
+        self.assertEqual(self.s2.y, 0)
+        self.assertEqual(self.s3.y, 0)
+        self.assertEqual(self.s4.y, 2)
+
+    def test_10_09_id_check(self):
+        """Check id"""
+
+        self.assertEqual(self.s1.id, 11)
+        self.assertEqual(self.s2.id, 12)
+        self.assertEqual(self.s3.id, 13)
+        self.assertEqual(self.s4.id, 14)
+
+    def test_10_10_x_error(self):
+        """Tests for errors with x"""
+
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            s = Square(1, "hello")
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            s = Square(1, 2.2)
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            s = Square(1, [2, 1])
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            s = Square(1, "hi")
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            s = Square(1, (3, 2))
+        with self.assertRaisesRegex(ValueError, "x must be >= 0"):
+            s = Square(1, -2)
+        with self.assertRaises(TypeError):
+            s = Square(1, None)
+        with self.assertRaises(TypeError):
+            s = Square(1, float('inf'))
+
+    def test_10_11_y_error(self):
+        """Tests for errors with y"""
+
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            s = Square(1, 1, "hello")
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            s = Square(1, 1, 2.2)
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            s = Square(1, 1, [2, 1])
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            s = Square(1, 1, "hi")
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            s = Square(1, 1, (3, 2))
+        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
+            s = Square(1, 1, -2)
+        with self.assertRaises(TypeError):
+            s = Square(1, 1, None)
+        with self.assertRaises(TypeError):
+            s = Square(1, 1, float('inf'))
+
     def test_11_00_size_check(self):
         """Checks to make sure size method works"""
 
@@ -97,6 +173,18 @@ class TestSquare(unittest.TestCase):
             s = Square(0)
         with self.assertRaisesRegex(ValueError, "width must be > 0"):
             s = Square(-2)
+        with self.assertRaises(TypeError):
+            s = Square(None)
+        with self.assertRaises(TypeError):
+            s = Square(float('inf'))
+
+    def test_11_02_size(self):
+        """Checks the size method"""
+
+        self.assertEqual(self.s1.size, 5)
+        self.assertEqual(self.s2.size, 7)
+        self.assertEqual(self.s3.size, 2)
+        self.assertEqual(self.s4.size, 3)
 
     def test_12_0_update_args(self):
         """Tests the update function for *args"""
