d63bf6560729bcbacb20ed407e4c90b32e48b39b
Marc Cavigli <mcavigli@gmail.com>
2019-07-16 06:29:53 +0000

    Added COLLATE

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,3 @@
 -- Coverts a database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
