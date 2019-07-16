68ae4eabe58c01e272995e0d7d50a1e346124d16
Marc Cavigli <mcavigli@gmail.com>
2019-07-16 22:58:07 +0000

    Changed to LEFT JOIN

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,8 @@
 -- Uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
 SELECT tv_genres.name AS name FROM tv_genres
-JOIN tv_show_genres
+LEFT JOIN tv_show_genres
      ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows
+LEFT JOIN tv_shows
      ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY name ASC;
