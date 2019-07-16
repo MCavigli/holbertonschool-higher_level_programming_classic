60d5cdc7d9f7cf2554b68c6396d9c7a46407f4d6
Marc Cavigli <mcavigli@gmail.com>
2019-07-16 06:49:15 +0000

    Pushed Task 19 to directory

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,5 @@
+-- Displays the top 3 of cities temperature during July and August ordered by temperature (descending)
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
