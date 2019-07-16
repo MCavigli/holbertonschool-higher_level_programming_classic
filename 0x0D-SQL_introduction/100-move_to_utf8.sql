900fa9140b565fc2b82d6adcef5a3486535ef3ea
Marc Cavigli <mcavigli@gmail.com>
2019-07-16 06:28:13 +0000

    Added a line for table coversion

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,3 @@
 -- Coverts a database to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8;
