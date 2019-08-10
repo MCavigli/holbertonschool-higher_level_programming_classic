e1334eded8c0e3ca1ee880fdfb1c1adc7ee81f45
Marc Cavigli <mcavigli@gmail.com>
2019-08-10 22:25:19 +0000

    Pushed Task 14 to directory

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Task 14 """
+
+if __name__ == "__main__":
+    """ """
+
+    from sys import argv
+    from sqlalchemy import (create_engine)
+    from model_state import Base, State
+    from model_city import Base, City
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state, city in session.query(State, City)\
+                              .filter(City.state_id == State.id)\
+                              .order_by(State.id)\
+                              .all():
+        print("{}: ({}) {}".format(state.name, city.state_id, city.name))
+    '''
+    result = session.query(Cities).join(States).all()
+    for row in result:
+        for st in row.
+    '''
+    session.close()
