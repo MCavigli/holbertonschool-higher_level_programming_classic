8222067e7e1627da98430d5f57e8cea047063cfd
Marc Cavigli <mcavigli@gmail.com>
2019-08-10 20:03:16 +0000

    Pushed Task 7 to directory

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Task 7 """
+
+if __name__ == "__main__":
+    """ """
+
+    from sys import argv
+    from sqlalchemy import (create_engine)
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # HERE: no SQL query, only objects!
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
