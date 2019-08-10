e8afe8f1ee82db196f8e369bcadaaa3d4e1349f1
Marc Cavigli <mcavigli@gmail.com>
2019-08-10 20:03:34 +0000

    Pushed Task 8 to directory

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Task 8 """
+
+if __name__ == "__main__":
+    """ """
+
+    from sys import argv
+    from sqlalchemy import (create_engine)
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    firstState = session.query(State).first()
+    print("{}: {}".format(firstState.id, firstState.name))
+    session.close()
