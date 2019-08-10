86e7c5be85e25ff41565f49b594fc48211a112fb
Marc Cavigli <mcavigli@gmail.com>
2019-08-10 20:04:07 +0000

    Pushed Task 10 to directory

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Task 10 """
+
+if __name__ == "__main__":
+    """ """
+
+    from sys import argv
+    from sqlalchemy import (create_engine)
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    flag = 0
+    for state in session.query(State).order_by(State.id).all():
+        if state.name == argv[4]:
+            flag = 1
+            print("{}".format(state.id))
+    if flag == 0:
+        print("Not found")
+
+    session.close()
