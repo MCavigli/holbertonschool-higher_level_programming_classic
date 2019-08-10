d74c0f9cb4c870daffaa546be4fb6f40ae7aa1ae
Marc Cavigli <mcavigli@gmail.com>
2019-08-10 20:00:00 +0000

    Pushed Task 2 to directory

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# takes in an argument and displays all values in the states table of
+# hbtn_0e_0_usa where name matches the argument
+
+
+def getStates(userName, passWord, dbName, stateName):
+    """ Accesses database hbtn_0e_0_usa and grabs states that begin with 'N'.
+    ARGS:
+        userName: the username
+        passWord: the password
+        dbName: the name of the database to access
+        stateName: the name of the state to check
+    """
+
+    import MySQLdb
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=userName,
+        passwd=passWord,
+        db=dbName,
+        charset="utf8"
+    )
+
+    cur = db.cursor()
+    sqlCommand = "SELECT * FROM states WHERE name LIKE '{}'".format(stateName)
+    cur.execute(sqlCommand)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    """ Take in arguments and passes to get states from db """
+    from sys import argv
+
+    getStates(argv[1], argv[2], argv[3], argv[4])
