f68a01640e853f97a30a32c6e9a0fa7c624d1302
Marc Cavigli <mcavigli@gmail.com>
2019-08-10 20:03:52 +0000

    Pushed Task 9 to directory

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Task 9 """
+
+if __name__ == "__main__":
+    """ """
+
+    from sys import argv
+    from sqlalchemy import (create_engine)
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id).all():
+        for char in range(len(state.name)):
+            if state.name[char] == 'a':
+                print("{}: {}".format(state.id, state.name))
+                break
+    session.close()
