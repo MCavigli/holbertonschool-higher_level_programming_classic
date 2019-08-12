9d2b467b8676a099ac0d215684a70239b7c7bb9d
Marc Cavigli <mcavigli@gmail.com>
2019-08-12 05:21:18 +0000

    Fixed typo

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -16,4 +16,5 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete-orphan')
+    cities = relationship('City', backref='state',
+                          cascade='all, delete-orphan')
