f353555378838afbf0dba0fe92cce2f441f33b12
Marc Cavigli <mcavigli@gmail.com>
2019-08-12 05:20:25 +0000

    Added library back

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -5,6 +5,7 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from relationship_city import City
 
 Base = declarative_base()
 
