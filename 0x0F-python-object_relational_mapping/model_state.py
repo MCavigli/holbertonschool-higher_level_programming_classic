8929b8bde106fc0c04d9e7bc65a6a736dcba1429
Marc Cavigli <mcavigli@gmail.com>
2019-08-15 23:54:40 +0000

    Removed extra code that should appear later in project:

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,7 +5,6 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from relationship_city import City
 
 Base = declarative_base()
 
@@ -16,4 +15,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state')
