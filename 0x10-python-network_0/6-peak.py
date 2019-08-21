3a3c0289e0b1db8725943814dbdebacf9e60badb
Marc Cavigli <mcavigli@gmail.com>
2019-08-21 03:43:03 +0000

    Fixed typo

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,15 +6,11 @@ def find_peak(list_of_integers):
     """ finds the peak of an unsorted list of ints """
     if not list_of_integers:
         return None
-    length = len(list_of_integers)
-    sp = length // 2
-    if length == 1:
-        return list_of_integers[sp]
-    if length == 2:
-        if list_of_integers[sp] > list_of_integers[sp - 1]:
-            return list_of_integers[sp]
-        else:
-            return list_of_integers[sp - 1]
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    end = len(list_of_integers) - 1
+    start = 0
+    return rfind(list_of_integers, start, end)
 
 
 def rfind(list_of_integers, start, end):
@@ -22,9 +18,6 @@ def rfind(list_of_integers, start, end):
     if start == end:
         return (list_of_integers[end])
     mid = (start + end) // 2
-    if mid == len(list_of_integers) - 1 or mid == 0:
-        return list_of_integers[mid]
-
-    elif list_of_integers[mid] > list_of_integers[mid + 1]:
+    if list_of_integers[mid] > list_of_integers[mid + 1]:
         return rfind(list_of_integers, start, mid)
     return rfind(list_of_integers, mid + 1, end)
