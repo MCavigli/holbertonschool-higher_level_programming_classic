4cb87cc664cd233e2bdcab2d1484e4436bcb3325
Marc Cavigli <mcavigli@gmail.com>
2019-08-26 23:35:40 +0000

    Initial push

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Module that posts request of letter to specific website
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    # posts request of letter to specific website
+    url = 'http://172.31.54.208:47095/search_user'
+    try:
+        q = {'q': sys.argv[1]}
+        r = requests.post(url, data=q)
+        if r.json():
+            print("[{}] {}".format(r.json()['id'], r.json()['name']))
+        else:
+            print("No result")
+    except IndexError:
+        print("No result")
+    except TypeError:
+        print("Not a valid JSON")
