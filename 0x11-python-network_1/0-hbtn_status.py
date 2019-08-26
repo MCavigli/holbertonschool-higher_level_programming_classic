4db427eb96cd20f9de016d241efbea93e3d0b7ea
Marc Cavigli <mcavigli@gmail.com>
2019-08-26 19:25:32 +0000

    Added necessary print line

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         html = response.read()
+    print("Body response:")
     print("    - type: {}".format(type(html)))
     print("    - content: {}".format(html))
     print("    - utf8 content: {}".format(html.decode("utf-8")))
