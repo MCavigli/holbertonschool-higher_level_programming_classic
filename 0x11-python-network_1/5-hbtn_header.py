cc8844ccb66c6220b301ea619a3f8742a817ae2b
Marc Cavigli <mcavigli@gmail.com>
2019-08-26 21:43:34 +0000

    Initial push

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Module that displays value of variable X-Request-Id
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    # displays value of variable X-Request-Id
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
