7b18069768a10a99b67c9216d254bbcf99f8cbfb
Marc Cavigli <mcavigli@gmail.com>
2019-08-26 21:16:34 +0000

    Initial push

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Module with code that mangages error code
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    # displays body of response and manages error code
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as request:
+            page = request.read()
+        print(page.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(e.code)
