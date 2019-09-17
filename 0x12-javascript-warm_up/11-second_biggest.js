2cdb0ea04f5f0bc623a1a1244b12917583543419
Marc Cavigli <mcavigli@gmail.com>
2019-09-17 21:28:32 +0000

    Refactored code. Much simpler and shorter now. Incorporated slice function as well as sort algo found online.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,14 +4,6 @@ const len = process.argv.length;
 if (len < 4) {
   console.log(0);
 } else {
-  const newArray = [];
-  let i = 2;
-  let j = 0;
-  for (; i < len; i++) {
-    newArray[j] = parseInt(process.argv[i]);
-    j++;
-  }
-  newArray.sort();
-  const arrayLen = newArray.length;
-  console.log(newArray[arrayLen - 2]);
+  const numArray = process.argv.sort((a, b) => a - b);
+  console.log(numArray[numArray.length - 2]);
 }
