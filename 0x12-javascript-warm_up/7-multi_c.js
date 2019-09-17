277c7bda3bc0992e1111e14bd3944776e7f278cb
Marc Cavigli <mcavigli@gmail.com>
2019-09-17 20:02:08 +0000

    Added conditional to account for negative integers

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,11 +2,12 @@
 
 const x = process.argv[2];
 let i = 0;
-if (i < x) {
+if (i < x && x >= 0) {
   while (i < x) {
     console.log('C is fun');
     i++;
   }
+} else if (x < 0) {
 } else {
   console.log('Missing number of occurrences');
 }
