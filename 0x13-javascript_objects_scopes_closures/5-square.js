8b182c545cc06127f7e0597fb2d0359dda7e6ab9
Marc Cavigli <mcavigli@gmail.com>
2019-09-23 20:27:08 +0000

    Put size declaration inside of super()

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -3,10 +3,7 @@
 const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
-    super();
-    this.width = size;
-    this.height = size;
+    super(size, size);
   }
 }
-
 module.exports = Square;
