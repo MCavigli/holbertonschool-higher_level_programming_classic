ee652d274016ee348e44e20987000c5035c89dde
Marc Cavigli <mcavigli@gmail.com>
2019-09-24 21:56:58 +0000

    Removed hardcode

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const url = process.argv[2];
-request(url, function (error, response, body) {
+request.get(url, function (error, response, body) {
   if (error) {
     console.error(error);
   }
@@ -10,7 +10,7 @@ request(url, function (error, response, body) {
   let count = 0;
   for (let i = 0; i < path.length; i++) {
     for (let j = 0; j < path[i].characters.length; j++) {
-      if (path[i].characters[j] === 'https://swapi.co/api/people/18/') {
+      if (path[i].characters[j].endsWith('18/')) {
         count++;
       }
     }
