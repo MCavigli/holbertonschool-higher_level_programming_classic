44b8503cf3be609c84c26f33ecb2735a05e37423
Marc Cavigli <mcavigli@gmail.com>
2019-09-24 19:57:12 +0000

    Moved around conditionals and changed function syntax

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-  if (err) {
+fs.readFile(process.argv[2], 'utf-8', function (err, data) {
+  if (!err) {
+    console.log(data);
+  } else {
     console.error(err);
-    return;
   }
-  console.log(data);
 });
